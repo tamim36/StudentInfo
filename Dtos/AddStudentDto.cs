805cb9fe136edb2741832409c77ced9ea1fe70c9
tamim36 <tamimarefinanik@gmail.com>
2021-03-11 07:06:07 +0600

    finalize everything

--- Dtos/AddStudentDto.cs
+++ Dtos/AddStudentDto.cs
@@ -11,6 +11,14 @@ namespace Dtos
         public string FullName { get; set; }
         public int ProgramsId { get; set; }
         public IFormFile File { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+        public string GuardianName { get; set; }
+        public string ContactNo { get; set; }
+        public string GuardianContactNo { get; set; }
+        public string NationalId { get; set; }
+        public string Email { get; set; }
+        public string PresentAddress { get; set; }
         public string AcademicInfo { get; set; }
     }
 }
